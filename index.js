ed505f59d74c39e716035abc43b3961ab706a2f0
Muhammad Zeeshan Akram <zeeshansiddiqui9790@gmail.com>
2024-10-23 00:01:30 +0500

    index.js commited

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app=require('./app');
 const db=require('./config/db');
 const ToDoModel=require('./model/todo_model');
 
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 10000;
 app.listen(port,()=>{
     
     console.log(`server is running on port ${port}`);
