6feccc14b98310146a78452892a66cabc943d467
Muhammad Zeeshan Akram <zeeshansiddiqui9790@gmail.com>
2024-10-23 19:04:05 +0500

    Backend  commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const db = require('./config/db'); // You can set up your MongoDB connection here
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
